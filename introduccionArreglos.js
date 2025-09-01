82c965b8484927e598974eebdddce5422d48d574
YahirAgustin <157630965+YahirAgustin@users.noreply.github.com>
2025-08-31 22:10:13 -0600

    Add files via upload

---
+++ introduccionArreglos.js
@@ -0,0 +1,22 @@
+
+
+console.log("Hola Mundo");
+
+//Declarar Variables 
+var a = 15
+var nombre = "Yahir"
+
+console.log(a);
+console.log(nombre);
+
+//Declara arreglos 
+
+let arreglon = [];
+let arreglo = new Array();
+
+//Inicializar Arreglo
+let cursos = ['HTML','CSS','JAVASCRIPT'];
+
+//Acceder Arreglo 
+console.log(cursos);
+console.log(cursos[1])
