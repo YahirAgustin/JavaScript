7dc3af71420cd78da8670b3e1f6095252466d422
YahirAgustin <157630965+YahirAgustin@users.noreply.github.com>
2025-09-08 22:23:42 -0600

    Recorrer Arreglos por Filas

---
+++ recorrerporfilas.js
@@ -0,0 +1,14 @@
+
+// Declarar la matriz
+let Arreglo = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+// Recorrer la matriz con dos bucles for
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        console.log(Arreglo[i][j] + " "); // imprime en la misma línea
+    }
+}
